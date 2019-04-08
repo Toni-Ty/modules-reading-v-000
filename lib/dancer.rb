07d0c80e326a62ebade2b1176f8cdbfba96b1557
Toni-Ty <antonique2479@yahoo.com>
2019-04-08 06:04:54 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -5,6 +5,7 @@ require_relative './fancy_dance.rb'
 class Dancer
   extend FancyDance::ClassMethods #:: This syntax references the parent and child relationship of the nested modules.
   include FancyDance::InstanceMethods
+end
 
 attr_accessor :name
 
@@ -12,3 +13,4 @@ def initialize(name)
   @name = name
   end
 end
+
