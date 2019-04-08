d8e1bdd17283c53fc7ea028d1ae7d07570b1587e
Toni-Ty <antonique2479@yahoo.com>
2019-04-08 06:18:37 +0000

    Automatically backed up by Learn

--- lib/class_methods_module.rb
+++ lib/class_methods_module.rb
@@ -1,8 +1,8 @@
 # Modules allow us to collect and bundle a group of methods
 # and make those methods available to any number of classes.
 module MetaDancing
-
-def metadata
-  "This class produces objects that love to dance"
+  def metadata
+    "This class produces objects that love to dance."
   end
 end
+
