445db330876c844d77c14f535d0dd4c5a6a684d3
Toni-Ty <antonique2479@yahoo.com>
2019-04-08 06:03:13 +0000

    Automatically backed up by Learn

--- lib/dance_module.rb
+++ lib/dance_module.rb
@@ -1,6 +1,8 @@
-module Dance
-  def twirl
-    "I'm twirling"
+module FancyDance
+  module InstanceMethods
+
+def twirl
+    "I'm twirling!"
 end
 
 def jump
@@ -13,5 +15,13 @@ end
 
 def take_a_bow
   "Thank you, thank you. It was a pleasure to dance for you all."
+  end
+end
+
+module ClassMethods
+
+def metadata
+  "This class produces objects that love to dance"
+    end
   end
 end
